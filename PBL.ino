2261b1b54a45d7ef8a49f77035376a47630f33bb
Arush <112827565+ArushYadlapati@users.noreply.github.com>
2024-06-14 13:01:38 -0700

    Update PBL.ino

--- PBL.ino
+++ PBL.ino
@@ -9,6 +9,7 @@ int sensorValue;
 int digitalValue;
 int airCount = 0;
 int buttonPressed = 0;
+int sensorTrigger = 350;
 
 static const int DHT_SENSOR_PIN = 3;
 DHT_Async dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
@@ -16,7 +17,7 @@ DHT_Async dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 void setup() {
     pinMode(2, INPUT);
     pinMode(3, OUTPUT);
-    pinMode(4, INPUT);    
+    pinMode(4, INPUT);
     pinMode(5, OUTPUT);
     pinMode(6, OUTPUT);
     pinMode(10, INPUT_PULLUP);
@@ -56,49 +57,46 @@ void loop() {
     
     sensorValue = analogRead(0);
     digitalValue = digitalRead(2);
-    analogWrite(5, sensorValue);
-    analogWrite(6, digitalWrite);
-    Serial.println(airCount);
+    
+    Serial.println(buttonPressed);
     Serial.println(sensorValue);
+    Serial.println(airCount);
 
     delay(100);
 
-    if (airCount > 20) {
-        if (sensorValue > 300) {
-          if (buttonPressed == 1){
-            if (digitalRead(10) == HIGH) {
-              Serial.println(sensorValue);
-              if (sensorValue > 99){
-                Serial.print("");
-              }
-
-              else {
-                Serial.print(" ");
-              }
-              
-              Serial.print(buttonPressed);
-              tone(3, 32);
-              delay(1000);
-              noTone(3);
-            }
+    if ((airCount > 20) and (sensorValue > sensorTrigger) and (buttonPressed == 1)) {
+        if (buttonPressed == 1) {      
+            Serial.println(sensorValue);
+            Serial.print(buttonPressed);
+            tone(9, 32);
+            delay(1000);
+            noTone(9);
           }
-        }
     }
 
-    if ((sensorValue > 50) and airCount < 50) {
+    if ((sensorValue > sensorTrigger) and airCount < 50) {
         airCount ++;  
     }
     
     else {
-      noTone(3);
-      if (sensorValue < 300) {
-        if (airCount > 0) {
+      noTone(9);
+      if ((sensorValue < 250) and (airCount > 0)) {
           airCount = airCount - 1;
-        }
+        
       }
     }
     
     if (measure_environment(&temperature, &humidity)) {
+        if (digitalRead(10) == LOW) {
+          if (buttonPressed == 0) {
+            buttonPressed = 1;    
+          }
+          else {
+            buttonPressed = 0;
+        }
+          delay(200);
+        }
+      
         Serial.print("T = ");
         Serial.print(temperature, 1);
         Serial.print(" deg. C, H = ");
@@ -116,21 +114,26 @@ void loop() {
         lcd.print(temperature - 3.5);
         lcd.print("C");
         lcd.print(" ");
-        lcd.print(sensorValue);
+
 
         if (sensorValue < 200) {
+          lcd.print(sensorValue);
           lcd.print(": Good");  
         }
 
         else if (sensorValue < 250) {
+          lcd.print(" ");
+          lcd.print(sensorValue);
           lcd.print(": Ok");  
         }
 
         else if (sensorValue < 275) {
+          lcd.print(sensorValue);
           lcd.print(": Poor");  
         }
 
         else {
+          lcd.print(sensorValue);
           lcd.print(": Bad");  
         }
         
@@ -148,7 +151,16 @@ void loop() {
         
         else {
           noTone(3);
-        }  
+        }
+        
+        if (digitalRead(10) == LOW){
+          if (buttonPressed == 0){
+            buttonPressed = 1;    
+          }
+          else {
+            buttonPressed = 0;
+        }
+          delay(200);
+        }
     }
-    
 }
